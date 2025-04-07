5c6258cbac25be58d1fafc10de33573132209677
LimsaJoshi <limsajoshi@gmail.com>
2025-04-07 13:25:27 +0530

    update2

--- src/test/java/com/batch2/artifact1/LoginControllerTest.java
+++ src/test/java/com/batch2/artifact1/LoginControllerTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.when;
@@ -21,7 +20,7 @@ public class LoginControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    
     private LoginService loginService;
 
     @Test
