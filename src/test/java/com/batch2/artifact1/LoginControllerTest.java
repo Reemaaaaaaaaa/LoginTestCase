c74951f4ea6007ad8d9f8c6892ca01bcb6c7809b
LimsaJoshi <limsajoshi@gmail.com>
2025-04-16 08:28:14 +0530

    test update 1

--- src/test/java/com/batch2/artifact1/LoginControllerTest.java
+++ src/test/java/com/batch2/artifact1/LoginControllerTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.Mockito.when;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -20,7 +21,7 @@ public class LoginControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Mock
+    @MockBean
     private LoginService loginService;
 
     @Test
